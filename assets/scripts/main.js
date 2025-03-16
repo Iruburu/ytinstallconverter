350b4da580a219d0b38c0e10b6a61dad3708a24c
Iruburu <115943161+Iruburu@users.noreply.github.com>
2025-03-16 21:13:18 +0000

    modified:   assets/scripts/main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,150 +1,137 @@
-// Busca o formulário onde vai ser colocados as URLs
-const formUrl = document.getElementById('form_url')
+class newApp {
+  constructor() {
+    // Busca o formulário onde vai ser colocados as URLs
+    this.formUrl = document.getElementById("form_url");
 
-const buttonFetch = document.getElementById('fetch_List_URLs')
+    this.buttonFetch = document.getElementById("fetch_List_URLs");
 
-const HTMLListURLs = document.getElementById('list_urls')
+    this.HTMLListURLs = document.getElementById("list_urls");
 
-// Uma lista onde será guardado as URLs selecionadas
-const listURLs = []
-
-// Funcção para um novo objeto interativo para melhor manuzeio da URL
-class NewUrl {
-    constructor(url, method) {
-        this.number = listURLs.length + 1
-        this.URL = url
-        this.method = method
-    }
-
-    edit() {
-        return false
-    }
+    // Uma lista onde será guardado as URLs selecionadas
+    this.listURLs = [];
+  }
 }
 
-// Função Listener interavel pelo formUrl que é executado quando um submit é chamado
-formUrl.addEventListener("submit", function (event) {
+newApp.prototype.startForm = (event) => {
+  // Evitar que a pagina seja atualizada quando o sumit for chamado
+  event.preventDefault();
 
-    // Evitar que a pagina seja atualizada quando o sumit for chamado
-    event.preventDefault()
+  // Função para pegar os valos preenchidos dentro do formulário
+  const getvalues = function (id, clear = true, type = "id") {
+    /* Essa função serve para buscar os valores preenchidos dentro do formulário, coms as seguintes ações:
+        - Pegar id do formulário, para não haver erro na busca
+        - buscar os elemento que foi especificado
+        - Pegar o valor preenchido e remover espaços laterais */
 
-    // Função para pegar os valos preenchidos dentro do formulário
-    const getvalues = function (id, clear = true, type = 'id') {
+    const idForm = app.formUrl.id;
+    const search = type == "class" ? `#${idForm} .${id}` : `#${idForm} #${id}`;
+    const input = document.querySelector(search);
 
-        /* Essa função serve para buscar os valores preenchidos dentro do formulário, coms as seguintes ações:
-            - Pegar id do formulário, para não haver erro na busca
-            - buscar os elemento que foi especificado
-            - Pegar o valor preenchido e remover espaços laterais */
+    // Verifica se o elemento buscado exite e faz tratamento de erros
+    if (!input)
+      throw new Error(
+        "O elemento " +
+          id +
+          " não foi encontrado. Por favor, verifique o " +
+          (type == "class" ? "." + id : "#" + id) +
+          " e tente novamente."
+      );
 
-        const idForm = formUrl.id
-        const search = type == 'class' ? `#${idForm} .${id}` : `#${idForm} #${id}`
-        const input = document.querySelector(search)
+    const value = input.value.trim();
 
-        // Verifica se o elemento buscado exite e faz tratamento de erros
-        if (!input) throw new Error(
-            "O elemento " + id + " não foi encontrado. Por favor, verifique o " +
-            (type == 'class' ? '.' + id : '#' + id) + " e tente novamente."
-        )
+    // Faz a limpeza do do elemento que foi buscado
+    if (clear) input.value = "";
 
-        const value = input.value.trim()
+    // Retorna valor buscado
+    return value;
+  };
 
-        // Faz a limpeza do do elemento que foi buscado
-        if (clear) input.value = ''
+  // Variáveis onde sera guardado os valores buscados
+  const inputUrl = getvalues("input_url"); // --> URL
+  const methodSelected = getvalues("method_selected", false); // --> Tipo de arquivo
 
-        // Retorna valor buscado
-        return value
-    }
+  // Objeto interativo da nova URL
+  const url = new app.newUrl(inputUrl, methodSelected);
 
-    // Variáveis oned sera guardado os valores buscados
-    const inputUrl = getvalues("input_url") // --> URL
-    const methodSelected = getvalues("method_selected", false) // --> Tipo de arquivo
+  // Adiciona a URl á lista
+  app.listURLs.push(url);
 
-    console.log(methodSelected)
+  app.drawListURLs(app.listURLs);
+};
 
-    // Objeto interativo da nova URL
-    const url = new NewUrl(inputUrl, methodSelected)
+newApp.prototype.newUrl = class {
+  constructor(url, method) {
+    this.index = app.listURLs.length + 1;
 
-    // Adiciona a URl á lista
-    listURLs.push(url)
+    this.URL = url;
 
-    drawListURLs(listURLs)
-    console.log(listURLs)
-})
+    this.method = method;
+  }
 
+  edit() {
+    return false;
+  }
+};
 
-async function fetchData(url, data, method = 'POST') {
+newApp.prototype.startApp = () => {
+  app.formUrl.addEventListener("submit", app.startForm);
+};
 
-    const body = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
+newApp.prototype.createTableRow = (data) => {
+  // faz a descontrusção do objeto
+  const { index, URL, method } = data;
 
-    try {
-        let response = await fetch(url, body);
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + 
-                response.statusText
-            );
-        }
-        let data = await response.json()
-        return data;
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error)
-    }
-}
+  // Criar elementos
+  const tr = document.createElement("tr");
+  tr.classList.add("url", "grid");
 
-// buttonFetch.addEventListener('click', function(event) {
-//     fetchData("http://localhost:1313/api/", listURLs)
-// })
+  const tdIndex = document.createElement("td");
+  tdIndex.classList.add("index");
+  tdIndex.textContent = index;
 
+  const tdUrl = document.createElement("td");
+  tdUrl.classList.add("url");
+  tdUrl.textContent = URL;
 
-function drawListURLs(urls) {
-    
-    const createTableRow = function(index, url, ext) {
-        // Criar elementos
-        const tr = document.createElement("tr");
-        tr.classList.add("url", "grid");
-        
-        const tdIndex = document.createElement("td");
-        tdIndex.classList.add("index");
-        tdIndex.textContent = index;
-        
-        const tdUrl = document.createElement("td");
-        tdUrl.classList.add("url");
-        tdUrl.textContent = url;
-        
-        const tdExt = document.createElement("td");
-        tdExt.classList.add("ext");
-        tdExt.textContent = ext;
-        
-        const tdRemove = document.createElement("td");
-        tdRemove.classList.add("Bu-remove");
-        tdRemove.setAttribute('value', index)
+  const tdExt = document.createElement("td");
+  tdExt.classList.add("ext");
+  tdExt.textContent = method;
 
-        tdRemove.addEventListener('click',function() {
-            tr.remove()
-        })
-        
-        // Adicionar elementos ao <tr>
-        tr.appendChild(tdIndex);
-        tr.appendChild(tdUrl);
-        tr.appendChild(tdExt);
-        tr.appendChild(tdRemove);
-        
-        return tr;
-    }
+  const tdRemove = document.createElement("td");
+  tdRemove.classList.add("Bu-remove");
+  tdRemove.setAttribute("value", index);
 
-    HTMLListURLs.innerHTML = ''
-    
-    const reverseUrl = urls.reverse()
+  tdRemove.addEventListener("click", function () {
+    app.deleteToUrl(index)
+    tr.remove();
+  });
 
-    reverseUrl.forEach(function (url) {
-        
-        HTMLListURLs.appendChild(
-            createTableRow(url.number, url.URL, url.method)
-        )
-    });
+  // Adicionar elementos ao <tr>
+  tr.appendChild(tdIndex);
+  tr.appendChild(tdUrl);
+  tr.appendChild(tdExt);
+  tr.appendChild(tdRemove);
+
+  return tr;
+};
+
+newApp.prototype.drawListURLs = (urls) => {
+  app.HTMLListURLs.innerHTML = "";
+
+  const listTables = []
+
+  urls.forEach((url) => 
+    listTables.push(app.createTableRow(url)));
+
+  listTables.reverse().forEach((newTable) => {
+    app.HTMLListURLs.appendChild(newTable);
+  });
+};
+
+newApp.prototype.deleteToUrl = (index) => {
 
 }
+
+const app = new newApp();
+
+app.startApp();
